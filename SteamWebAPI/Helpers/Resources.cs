3058b23f6d29c14700b2aea77a1e267312331e00
Dal1ght <sergeyk95@mail.ru>
2018-09-24 21:01:33 +0300

    Add resource helper

---
+++ SteamWebAPI/Helpers/Resources.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SteamWebAPI.Helpers
+{
+	public static class Resources
+	{
+		public static JObject GetCountries()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var resourceName = "SteamWebAPI.Resources.steam_countries.json";
+
+			JObject countries = null;
+
+			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				string result = reader.ReadToEnd();
+				countries = JObject.Parse(result);
+			}
+
+			return countries;
+		}
+	}
+}
